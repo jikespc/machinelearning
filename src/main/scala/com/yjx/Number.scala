a009423583703fd0e658082fd36667bbc9e2afc0
jikespc <2306873977@qq.com>
2022-01-11 17:46:42 +0800

    回归模型

--- src/main/scala/com/yjx/Number.scala
+++ src/main/scala/com/yjx/Number.scala
@@ -1,5 +1,7 @@
 package com.yjx
 
+import org.apache.spark.ml.linalg.Vectors
+import org.apache.spark.ml.regression.LinearRegression
 import org.apache.spark.sql.SparkSession
 
 object Number {
@@ -10,6 +12,29 @@ object Number {
     //将数据分为训练数据和测试数据
     val Array(trainingData,testData) = dateFrame.randomSplit(Array(0.7, 0.3))
     //创建线性回归
+    val linearRegression = new LinearRegression()
+      .setMaxIter(10)
+      .setRegParam(0.3)
+      .setElasticNetParam(0.8)
+      .setFitIntercept(true)
+    //开始适配模型
+    val linearRegressionModel = linearRegression.fit(trainingData)
+    println("线性回归系数：" + linearRegressionModel.coefficients)
+    println("线性回归截断值：" + linearRegressionModel.intercept)
+
+    //获取模型的综合评估信息
+    val summary = linearRegressionModel.summary
+    println("根据训练用例" + summary.rootMeanSquaredError)
+
+    //预估值
+    println("计算向量(1,2,3)的预估值" + linearRegressionModel.predict(Vectors.dense(1, 2, 3)))
+
+    //根据测试数据获取模型的评估值
+    val regressionSummary = linearRegressionModel.evaluate(testData)
+    println("根据测试用例获取：" + regressionSummary.rootMeanSquaredError)
+
+    //保存模型
+    //linearRegressionModel.save("models/number_" + UUID.randomUUID())
 
   }
 }
